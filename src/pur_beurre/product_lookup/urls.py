443f6f0460ece15db60d2ae359f7de0c03bfdc29
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-01-21 10:44:46 +0100

    Add Navbar and footer
    Responsive included.

--- src/pur_beurre/product_lookup/urls.py
+++ src/pur_beurre/product_lookup/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProductLookupView
 
+app_name = 'product_lookup'
+
 urlpatterns = [
     path('', ProductLookupView.as_view(), name='product_lookup'),
 ]
