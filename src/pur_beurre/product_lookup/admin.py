96d78894fb08cc33f1c169c98e3f100a41b2b41d
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-02-04 13:15:41 +0100

    Create Product and Favorites models

--- src/pur_beurre/product_lookup/admin.py
+++ src/pur_beurre/product_lookup/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Products, Favorites
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_url', 'product_image',
+                    'product_nutriscore', 'product_category')
+    list_filter = ('product_name', 'product_nutriscore', 'product_category')
+    search_fields = ('product_name', 'product_nutriscore', 'product_category')
+
+
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product')
+    list_filter = ('user', 'product')
+    search_fields = ('user', 'product')
+
+
+admin.site.register(Products, ProductsAdmin)
+admin.site.register(Favorites, FavoritesAdmin)
--- src/pur_beurre/product_lookup/models.py
+++ src/pur_beurre/product_lookup/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Products(models.Model):
+    """Represents a product from OpenFoodFacts"""
+
+    product_name = models.CharField(max_length=200)
+    product_url = models.URLField(max_length=200)
+    product_image = models.URLField(max_length=200)
+    product_nutriscore = models.CharField(max_length=1)
+    product_category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = "Produit"
+        verbose_name_plural = "Produits"
+
+
+class Favorites(models.Model):
+    """Represents a favorite product"""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('product_lookup.Products',
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = "Favori"
+        verbose_name_plural = "Favoris"
